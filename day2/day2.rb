ca6d503ff8a3ff235d1cfa024ab6f460cd05f059
Jonathan Karlsson <karlsjona@gmail.com>
2023-12-02 08:08:20 +0100

    Cleanup day 2

--- day2/day2.rb
+++ day2/day2.rb
@@ -3,7 +3,6 @@ data = File.read('input')
 puts 'Day 2'
 puts 'Part 1'
 
-# 12 red cubes, 13 green cubes, and 14 blue cubes
 def check_game(game)
   max = { 'red' => 12, 'green' => 13, 'blue' => 14 }
   game.split(';').all? do |r|
@@ -17,30 +16,27 @@ end
 
 def game_value(line)
   num_string, game = line.split ':'
-  if check_game(game)
+  if check_game game
     num_string.split[1].to_i
   else
     0
   end
 end
 
-values = data.lines.map { |l| game_value l }
-puts values.sum
+puts(data.lines.sum { |l| game_value l })
 
 puts 'Part 2'
 
 def game_power(line)
   _, game = line.split ':'
-  max = {}
-  game.split(';').each do |r|
-    r.split(',').each do |s|
+  max = game.split(';').each_with_object({}) do |round, acc|
+    round.split(',').each do |s|
       num_string, color = s.split
       num = num_string.to_i
-      max[color] = [max[color] || 0, num].max
+      acc[color] = [acc[color] || 0, num].max
     end
   end
   max.values.inject(:*)
 end
 
-powers = data.lines.map {|l| game_power l }
-puts powers.sum
+puts(data.lines.sum { |l| game_power l })
