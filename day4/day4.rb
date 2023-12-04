ec4ee1abcc962fe27e320774355141a54982f8d0
Jonathan Karlsson <karlsjona@gmail.com>
2023-12-04 17:57:07 +0100

    Day 4 complete and cleaned

--- day4/day4.rb
+++ day4/day4.rb
@@ -1,22 +1,33 @@
 require 'set'
 
 def parse_numbers(string)
-  string.split.map { |s| Integer(s) }.to_set
+  string.split.to_set { |s| Integer(s) }
 end
 
-data = File.read('input')
-puts 'Day 4'
-puts 'Part 1'
-res = data.lines.sum do |line|
+def compute_score(line)
   line.sub!(/^Card *\d+: /, '')
   winning, ours = line.split('|')
-  intersection = (parse_numbers(winning) & parse_numbers(ours))
-  num_winning = intersection.size
-  if num_winning != 0
-    val = 2 ** (num_winning - 1)
-    val
-  else
-    0
-  end
+  (parse_numbers(winning) & parse_numbers(ours)).size
+end
+
+puts 'Day 4'
+puts 'Part 1'
+data = File.read('input')
+res = data.lines.sum do |line|
+  score = compute_score(line)
+  if score.nonzero? then 2**(score - 1) else 0 end
 end
 puts res
+
+puts 'Part 2'
+num_cards = [1]
+data.lines.each_with_index do |line, index|
+  num_cards[index] = 1 if num_cards[index].nil?
+  score = compute_score line
+  range = (index + 1)..(index + score)
+  range.each do |i|
+    existing_value = num_cards[i] || 1
+    num_cards[i] = existing_value + num_cards[index]
+  end
+end
+puts num_cards.sum
